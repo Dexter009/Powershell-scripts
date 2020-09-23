24f95984ee0d2f2c48b5c016859e4f069bb0c3c2
Dhiraz Gazurel <dhirazgazurel@gmail.com>
2020-09-23 19:17:36 +1000

    Initial Commit
    accesschecker.ps1 for file access check and serviceaccess.ps1 for services access check in windows

---
+++ accesschecker.ps1
@@ -0,0 +1,8 @@
+﻿function Get-Permissions ($folder) {
+  (get-acl $folder).access | select `
+		@{Label="Identity";Expression={$_.IdentityReference}}, `
+		@{Label="Right";Expression={$_.FileSystemRights}}, `
+		@{Label="Access";Expression={$_.AccessControlType}}| ft -auto
+		}
+
+Thank you!!!
---
+++ serviceaccess.ps1
@@ -0,0 +1,20 @@
+﻿$services = get-wmiobject -query 'select * from win32_service'
+foreach ($service in $services) {
+    $path=$service.Pathname
+    $path
+    if (-not( test-path $path -ea silentlycontinue)) {
+        if ($Service.Pathname -match "(\""([^\""]+)\"")|((^[^\s]+)\s)|(^[^\s]+$)") {
+            $path = $matches[0] –replace """",""
+        }
+    }
+    $ServiceName = $service.Displayname
+    $rights = get-acl $path
+    Write "$ServiceName : Writable service"
+    foreach ($item in $rights.access){    
+        if ($item.FileSystemRights.tostring() -match "Modify|Full|Change") {
+            Write ("   "+$item.IdentityReference.value + " : "+$item.AccessControlType.tostring() + " : "+$item.FileSystemRights.tostring())
+        }        
+    }
+    Write "`r`n"
+    
+    }
